3b0221e4c1b586be7028aca8448f40b2e557f9a0
MARK HAMILTON <HAMILTON.MARK37@GMAIL.COM>
2025-02-19 20:13:18 -0800

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+function Footer (){
+    return(
+        <footer className="footer">
+            <p className="footer__text">Developed by Mark Hamilton</p>
+            <p className="footer__date">2025</p>
+        </footer>
+    );
+}
+
+export default Footer
