c12d78a4712173777066d6f66fd36837cd2613b9
MARK HAMILTON <HAMILTON.MARK37@GMAIL.COM>
2025-06-08 15:33:39 -0700

    fix delete button showing/not based on current user

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -5,25 +5,33 @@ import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 
 function ItemModal({ activeModal, onClose, card, onDeleteCard }) {
- const currentUser = useContext(CurrentUserContext)
- const isOwn = card.owner === currentUser._id
- const itemDeleteButtonClassName = (
-    `modal__delete-button ${isOwn ? '' : 'modal__delete-button_hidden'}`
- );
+    const currentUser = useContext(CurrentUserContext)
+    console.log("Card name:", card.name);
+    console.log("Card owner:", card.owner);
+    console.log("Current user:", currentUser._id);
+
+
+    const isOwn = card.owner === currentUser._id;
+    const itemDeleteButtonClassName = (
+        `modal__delete-btn ${isOwn ? '' : 'modal__delete-button_hidden'}`
+    );
     return (
-        <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-            <div className="modal__content modal__content_type_image">
-                <button onClick={onClose} type="button" className="modal__close">
-                    <img src={close} alt="close icon" />
-                </button>
-                <img src={card.imageUrl} alt={card.name} className="modal__image" />
-                <div className="modal__footer">
-                    <h2 className="modal__caption">{card.name}</h2>
-                    <p className="modal__weather">Weather: {card.weather}</p>
+        <>
+            <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+                <div className="modal__content modal__content_type_image">
+                    <button onClick={onClose} type="button" className="modal__close">
+                        <img src={close} alt="close icon" />
+                    </button>
+                    <img src={card.imageUrl} alt={card.name} className="modal__image" />
+                    <div className="modal__footer">
+                        <h2 className="modal__caption">{card.name}</h2>
+                        <p className="modal__weather">Weather: {card.weather}</p>
+                    </div>
+                    <button onClick={() => onDeleteCard(card._id)} type="button" className={itemDeleteButtonClassName}>Delete item</button>
                 </div>
-                <button onClick={() => onDeleteCard(card._id)} type="button" className={itemDeleteButtonClassName}>Delete item</button>
             </div>
-        </div>
+
+        </>
 
     )
 
